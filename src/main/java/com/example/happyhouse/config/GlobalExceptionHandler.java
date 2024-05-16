57fc62d123b15c4b29e1b569f5623860161a157b
Seojeonguk <junguk7880@naver.com>
2024-05-17 04:32:29 +0900

    Add global exception handler
    This will add global exception handler.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ src/main/java/com/example/happyhouse/config/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.happyhouse.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> BadRequestException(final Exception ex) {
+        log.warn("error", ex);
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAll(final Exception ex) {
+        log.info(ex.getClass().getName());
+        log.error("error", ex);
+        return ResponseEntity.internalServerError().body(ex.getMessage());
+    }
+
+}
