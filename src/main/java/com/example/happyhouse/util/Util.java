baeb8e8f44dba07e67b2b2c136195d6ec146b666
Seojeonguk <junguk7880@naver.com>
2024-05-21 22:16:14 +0900

    Add isEmpty util function
    This will add a function to check if the object is empty.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ src/main/java/com/example/happyhouse/util/Util.java
@@ -0,0 +1,27 @@
+package com.example.happyhouse.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Util {
+    /**
+     * Checks if an object is empty or null. Supports String, Collection, Map, and Array.
+     *
+     * @param obj the object to check
+     * @return true if the object is empty or null
+     */
+    public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        } else if (obj instanceof String) {
+            return ((String) obj).isEmpty();
+        } else if (obj instanceof Collection) {
+            return ((Collection<?>) obj).isEmpty();
+        } else if (obj instanceof Map) {
+            return ((Map<?, ?>) obj).isEmpty();
+        } else if (obj.getClass().isArray()) {
+            return ((Object[]) obj).length == 0;
+        }
+        return false;
+    }
+}
