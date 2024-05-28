eed29bcdddab27ef07b4d0b508b984d3ba36a77e
Seojeonguk <junguk7880@naver.com>
2024-05-29 05:20:12 +0900

    Fix header
    This will fix header.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
             "display: none ");
         // 로그아웃 버튼과 회원정보 버튼 살리기
         document.querySelector("#nav-logoutBtn").setAttribute("style",
-            "display: ");
+            "display: inline-block");
         document.querySelector("#nav-meminfoBtn").setAttribute("style",
-            "display: ");
+            "display: inline-block");
     } else {
         // 회원가입 버튼과 로그인 버튼 살리기
         document.querySelector("#nav-signupBtn")
-            .setAttribute("style", "display: ");
-        document.querySelector("#nav-loginBtn").setAttribute("style", "display: ");
+            .setAttribute("style", "display: inline-block");
+        document.querySelector("#nav-loginBtn").setAttribute("style", "display: inline-block");
         // 로그아웃 버튼과 회원정보 버튼 없애기
         document.querySelector("#nav-logoutBtn").setAttribute("style",
             "display: none ");
@@ -32,8 +32,8 @@ $(document).ready(function () {
 
         // 회원가입 버튼과 로그인 버튼 살리기
         document.querySelector("#nav-signupBtn")
-            .setAttribute("style", "display: ");
-        document.querySelector("#nav-loginBtn").setAttribute("style", "display: ");
+            .setAttribute("style", "display: inline-block");
+        document.querySelector("#nav-loginBtn").setAttribute("style", "display: inline-block");
         // 로그아웃 버튼과 회원정보 버튼 없애기
         document.querySelector("#nav-logoutBtn").setAttribute("style",
             "display: none ");
