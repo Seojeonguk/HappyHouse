98fcd10eed667826e8a9adb356499dc8e262b13a
Seojeonguk <junguk7880@naver.com>
2024-07-09 00:26:23 +0900

    Add index page
    This will add index page.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- src/main/java/com/example/happyhouse/domain/controller/PageController.java
+++ src/main/java/com/example/happyhouse/domain/controller/PageController.java
@@ -1,13 +1,19 @@
 package com.example.happyhouse.domain.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class PageController {
 
     @GetMapping(value = "/")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        return "index";
+    }
+
+    @GetMapping(value = "/index")
+    public String index(HttpServletRequest request) {
         return "index";
     }
 }
